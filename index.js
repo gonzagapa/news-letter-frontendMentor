c1db64deb3e6caec05655dcde919d8cd68d6e603
Gonzalo Anuar <gonzalo.anuar13@outlook.com>
2025-01-19 21:30:50 -0600

    Add initial project files including HTML structure, CSS styles, JavaScript functionality, and SVG icons

---
+++ index.js
@@ -0,0 +1,45 @@
+const form = document.forms['form-pop-up'];
+const input = form.email;
+const popUp = document.querySelector('.pop-up.principal');
+const confirmationPopUp = document.querySelector('.pop-up.bottom');
+const pValidation = document.querySelector('.p-validation');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!validationForm(input.value)) return;
+    popUp.classList.toggle('js-not-visibly');
+    confirmationPopUp.classList.toggle('js-not-visibly');
+});
+
+//This validates the email every time de user types a new value
+input.addEventListener('keydown', (event) => {
+    if (!validationForm(input.value)) {
+        pValidation.style.display = 'initial';
+        input.classList.add('invalid');
+    } else {
+        pValidation.style.display = 'none';
+        input.classList.remove('invalid');
+    }
+});
+
+//When the input lost focus, it validate that the email is correct
+input.addEventListener('blur', (event) => {
+    if (!validationForm(input.value)) {
+        pValidation.style.display = 'initial';
+        input.classList.add('invalid');
+    } else {
+        pValidation.style.display = 'none';
+        input.classList.remove('invalid');
+    }
+});
+
+
+document.getElementById('btn-dismiss').addEventListener('click', () => {
+    confirmationPopUp.classList.toggle('js-not-visibly');
+    popUp.classList.toggle('js-not-visibly');
+})
+
+function validationForm(value) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(value);
+}
